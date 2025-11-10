c34ce5108646cb58c3611e31a826efebb11a300a
Marius Schlegel <marius.schlegel@tu-ilmenau.de>
2025-11-10 11:44:09 +0100

    Adjust exercise 2

--- ex-2/tables.sql
+++ ex-2/tables.sql
@@ -1,65 +1,65 @@
-CREATE TABLE IF NOT EXISTS "Customers" (
-    "CNo"  INTEGER PRIMARY KEY,
-    "Name" VARCHAR NOT NULL
+CREATE TABLE IF NOT EXISTS Customers (
+    CNo  INTEGER PRIMARY KEY,
+    Name VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "Products" (
-    "PNo"         INTEGER PRIMARY KEY,
-    "Description" VARCHAR NOT NULL
+CREATE TABLE IF NOT EXISTS Products (
+    PNo         INTEGER PRIMARY KEY,
+    Description VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "Stores" (
-    "SID"     INTEGER PRIMARY KEY,
-    "Name"    VARCHAR NOT NULL,
-    "Address" VARCHAR
+CREATE TABLE IF NOT EXISTS Stores (
+    SID     INTEGER PRIMARY KEY,
+    Name    VARCHAR NOT NULL,
+    Address VARCHAR
 );
 
-CREATE TABLE IF NOT EXISTS "Sales" (
-    "RNo"  INTEGER PRIMARY KEY,
-    "SID"  INTEGER NOT NULL REFERENCES "Stores"("SID"),
-    "Date" DATE    NOT NULL,
-    "Time" TIME    NOT NULL,
-    "CNo"  INTEGER REFERENCES "Customers"("CNo")
+CREATE TABLE IF NOT EXISTS Sales (
+    RNo  INTEGER PRIMARY KEY,
+    SID  INTEGER NOT NULL REFERENCES Stores(SID),
+    Date DATE    NOT NULL,
+    Time TIME    NOT NULL,
+    CNo  INTEGER REFERENCES Customers(CNo)
 );
 
-CREATE TABLE IF NOT EXISTS "SpecialOffers" (
-    "SID" INTEGER NOT NULL,
-    "PNo" INTEGER NOT NULL,
-    PRIMARY KEY ("SID", "PNo"),
-    FOREIGN KEY ("SID") REFERENCES "Stores"("SID"),
-    FOREIGN KEY ("PNo") REFERENCES "Products"("PNo")
+CREATE TABLE IF NOT EXISTS SpecialOffers (
+    SID INTEGER NOT NULL,
+    PNo INTEGER NOT NULL,
+    PRIMARY KEY (SID, PNo),
+    FOREIGN KEY (SID) REFERENCES Stores(SID),
+    FOREIGN KEY (PNo) REFERENCES Products(PNo)
 );
 
-CREATE TABLE IF NOT EXISTS "Receipts" (
-    "RNo"      INTEGER NOT NULL,
-    "PNo"      INTEGER NOT NULL,
-    "Quantity" INTEGER NOT NULL CHECK ("Quantity" > 0),
-    PRIMARY KEY ("RNo", "PNo"),
-    FOREIGN KEY ("RNo") REFERENCES "Sales"("RNo"),
-    FOREIGN KEY ("PNo") REFERENCES "Products"("PNo")
+CREATE TABLE IF NOT EXISTS Receipts (
+    RNo      INTEGER NOT NULL,
+    PNo      INTEGER NOT NULL,
+    Quantity INTEGER NOT NULL CHECK (Quantity > 0),
+    PRIMARY KEY (RNo, PNo),
+    FOREIGN KEY (RNo) REFERENCES Sales(RNo),
+    FOREIGN KEY (PNo) REFERENCES Products(PNo)
 );
 
-INSERT INTO "Customers" ("CNo", "Name") VALUES
-    (123, 'Müller, F'),
-    (456, 'Abel, M'),
-    (789, 'Schulz, R'),
-    (109, 'Jahn, E'),
-    (403, 'Meier, T');
+INSERT INTO Customers (CNo, Name) VALUES
+    (123, 'Müller, F.'),
+    (456, 'Abel, M.'),
+    (789, 'Schulz, R.'),
+    (109, 'Jahn, E.'),
+    (403, 'Meier, T.');
 
-INSERT INTO "Products" ("PNo", "Description") VALUES
+INSERT INTO Products (PNo, Description) VALUES
     (45, 'Butter'),
     (56, 'Cake'),
     (11, 'Milk'),
     (67, 'Orange'),
     (13, 'Potatoes');
 
-INSERT INTO "Stores" ("SID", "Name", "Address") VALUES
+INSERT INTO Stores (SID, Name, Address) VALUES
     (27, 'Aldi',  'Hüttenholz'),
     (23, 'Netto', 'Herderstrasse'),
     (24, 'Tegut', 'Goethepassage'),
     (20, 'Rewe',  'Mühlgraben');
 
-INSERT INTO "Sales" ("RNo", "SID", "Date", "Time", "CNo") VALUES
+INSERT INTO Sales (RNo, SID, Date, Time, CNo) VALUES
     ( 1, 23, DATE '2025-09-27', TIME '08:13', 456),
     ( 3, 20, DATE '2025-09-30', TIME '09:59', 123),
     ( 5, 24, DATE '2025-10-18', TIME '12:07', 789),
@@ -67,7 +67,7 @@ INSERT INTO "Sales" ("RNo", "SID", "Date", "Time", "CNo") VALUES
     ( 9, 27, DATE '2025-10-19', TIME '21:01', 123),
     (17, 20, DATE '2025-11-03', TIME '11:34', 403);
 
-INSERT INTO "SpecialOffers" ("SID", "PNo") VALUES
+INSERT INTO SpecialOffers (SID, PNo) VALUES
     (27, 13),
     (27, 56),
     (23, 67),
@@ -76,7 +76,7 @@ INSERT INTO "SpecialOffers" ("SID", "PNo") VALUES
     (27, 67),
     (24, 67);
 
-INSERT INTO "Receipts" ("RNo", "PNo", "Quantity") VALUES
+INSERT INTO Receipts (RNo, PNo, Quantity) VALUES
     (1, 45, 2),
     (1, 67, 10),
     (3, 11, 2),
